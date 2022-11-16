6eeb69753dcba5ddfb64e821964f644d8fc95726
Karel Vervaeke <karel.vervaeke@conundra.eu>
2022-11-16 22:15:37 +0100

    Set up force graph

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as d3 from 'd3';
+import {Simulation} from "d3";
+import {ForceGraph} from "./forcegraph";
+import {MISERABLES} from "./miserables";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'openapi-graph';
+
+  private svg: any;
+  private width = 800;
+  private height = 600;
+  private margin = 10;
+
+  private simulation!: Simulation<any, any>;
+  private data = [{}, {}, {}, {}, {}];
+
+  ngOnInit(): void {
+    var graph = ForceGraph("#figure", MISERABLES,
+      {
+        nodeId: (d: any) => d.id,
+        nodeGroup: (d: any) => d.group,
+        nodeTitle: (d: any) => `${d.id}\n${d.group}`,
+        linkStrokeWidth: (l: any) => Math.sqrt(l.value),
+        width: this.width,
+        height: this.height,
+        //invalidation // a promise to stop the simulation when the cell is re-run
+      });
+
+
+  }
+
 }
